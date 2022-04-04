cd5186f191ae3709754b161dd2b0aaffc7b3911f
NQH1611 <88624093+NQH1611@users.noreply.github.com>
2022-04-04 18:29:53 +0700

    no message

--- src/assets/fake-data/products.js
+++ src/assets/fake-data/products.js
@@ -1,6 +1,6 @@
 const product_01_image_01 = require('../images/products/product-01 (1).jpg').default
 const product_01_image_02 = require('../images/products/product-01 (2).jpg').default
-// const product_01_image_03 = require('../images/products/product-01 (3).jpg').default
+    // const product_01_image_03 = require('../images/products/product-01 (3).jpg').default
 
 const product_02_image_01 = require('../images/products/product-02 (1).jpg').default
 const product_02_image_02 = require('../images/products/product-02 (2).jpg').default
@@ -35,202 +35,404 @@ const product_11_image_02 = require('../images/products/product-11 (2).jpg').def
 const product_12_image_01 = require('../images/products/product-12 (1).jpg').default
 const product_12_image_02 = require('../images/products/product-12 (2).jpg').default
 
-const products = [
-    {
-        title: "Áo thun Dinosaur 01",
-        price: '189000',
+const product_13_image_01 = require('../images/products/product-13 (1).jpg').default
+const product_13_image_02 = require('../images/products/product-13 (2).jpg').default
+
+const product_14_image_01 = require('../images/products/product-14 (1).jpg').default
+const product_14_image_02 = require('../images/products/product-14 (2).jpg').default
+
+const product_15_image_01 = require('../images/products/product-15 (1).jpg').default
+const product_15_image_02 = require('../images/products/product-15 (2).jpg').default
+
+const product_16_image_01 = require('../images/products/product-16 (1).jpg').default
+const product_16_image_02 = require('../images/products/product-16 (2).jpg').default
+
+const product_17_image_01 = require('../images/products/product-17 (1).jpg').default
+const product_17_image_02 = require('../images/products/product-17 (2).jpg').default
+
+const product_18_image_01 = require('../images/products/product-18 (1).jpg').default
+const product_18_image_02 = require('../images/products/product-18 (2).jpg').default
+
+const product_19_image_01 = require('../images/products/product-19 (1).jpg').default
+const product_19_image_02 = require('../images/products/product-19 (2).jpg').default
+
+const product_20_image_01 = require('../images/products/product-20 (1).jpg').default
+const product_20_image_02 = require('../images/products/product-20 (2).jpg').default
+
+const product_21_image_01 = require('../images/products/product-21 (1).jpg').default
+const product_21_image_02 = require('../images/products/product-21 (2).jpg').default
+
+const product_22_image_01 = require('../images/products/product-22 (1).jpg').default
+const product_22_image_02 = require('../images/products/product-22 (2).jpg').default
+
+const product_23_image_01 = require('../images/products/product-23 (1).jpg').default
+const product_23_image_02 = require('../images/products/product-23 (2).jpg').default
+
+const product_24_image_01 = require('../images/products/product-24 (1).jpg').default
+const product_24_image_02 = require('../images/products/product-24 (2).jpg').default
+
+const product_25_image_01 = require('../images/products/product-25 (1).jpg').default
+const product_25_image_02 = require('../images/products/product-25 (2).jpg').default
+
+const product_26_image_01 = require('../images/products/product-26 (1).jpg').default
+const product_26_image_02 = require('../images/products/product-26 (2).jpg').default
+
+const product_27_image_01 = require('../images/products/product-27 (1).jpg').default
+const product_27_image_02 = require('../images/products/product-27 (2).jpg').default
+
+const product_28_image_01 = require('../images/products/product-28 (1).jpg').default
+const product_28_image_02 = require('../images/products/product-28 (2).jpg').default
+
+const product_29_image_01 = require('../images/products/product-29 (1).jpg').default
+const product_29_image_02 = require('../images/products/product-29 (2).jpg').default
+
+const product_30_image_01 = require('../images/products/product-30 (1).jpg').default
+const product_30_image_02 = require('../images/products/product-30 (2).jpg').default
+
+const product_31_image_01 = require('../images/products/product-31 (1).jpg').default
+const product_31_image_02 = require('../images/products/product-31 (2).jpg').default
+
+const product_32_image_01 = require('../images/products/product-32 (1).jpg').default
+const product_32_image_02 = require('../images/products/product-32 (2).jpg').default
+
+
+const products = [{
+        title: "Nike Air VaporMax 2021 FK Men's Shoes.",
+        price: '5350000',
         image01: product_01_image_01,
         image02: product_01_image_02,
-        categorySlug: "ao-thun",
-        colors: ["white", "red", "orange"],
-        slug: "ao-thun-dinosaur-01",
-        size: ["s", "m", "l", "xl"]
+        categorySlug: "nike-shoes",
+        colors: ["white", "red"],
+        slug: "nike-air-vapormax-01",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo thun Dinosaur 02",
-        price: '159000',
+        title: "Nike Air Max 97 SE Men's Shoes.",
+        price: '3750000',
         image01: product_02_image_01,
         image02: product_02_image_02,
-        categorySlug: "ao-thun",
+        categorySlug: "nike-shose",
         colors: ["white", "red", "blue"],
-        slug: "ao-thun-dinosaur-02",
-        size: ["s", "m"]
+        slug: "nike-max-97",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo thun Dinosaur 03",
-        price: '190000',
+        title: "Nike Air Force 1 '07 WB Men's Shoes.",
+        price: '6300000',
         image01: product_03_image_01,
         image02: product_03_image_02,
-        categorySlug: "ao-thun",
+        categorySlug: "nike-shoes",
         colors: ["white", "red", "orange", "yellow"],
-        slug: "ao-thun-dinosaur-03",
-        size: ["m"]
+        slug: "nike-force-1-wb",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo thun Polo 04",
-        price: '194000',
+        title: "Nike Air Force 1 '07 Skeleton Men's Shoe.",
+        price: '3190000',
         image01: product_04_image_01,
         image02: product_04_image_02,
-        categorySlug: "ao-thun",
+        categorySlug: "nike-shoes",
         colors: ["white", "orange", "blue"],
-        slug: "ao-thun-polo-04",
-        size: ["xl"]
+        slug: "nike-force-1-skeleton",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo thun Polo 05",
-        price: '194000',
+        title: "Nike Air VaporMax Plus Men's Shoes.",
+        price: '2170000',
         image01: product_05_image_01,
         image02: product_05_image_02,
-        categorySlug: "ao-thun",
+        categorySlug: "nike-shoes",
         colors: ["white", "pink"],
-        slug: "ao-thun-polo-05",
-        size: ["xxl"]
+        slug: "nike-vapormax",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo thun Polo 06",
-        price: '194000',
+        title: "Nike Air Max 2021 SE Men's Shoes.",
+        price: '3150000',
         image01: product_06_image_01,
         image02: product_06_image_02,
-        categorySlug: "ao-thun",
+        categorySlug: "nike-shoes",
         colors: ["black"],
-        slug: "ao-thun-polo-06",
-        size: ["s", "m", "xl"]
+        slug: "nike-max-2021",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo somi caro 07",
-        price: '194000',
+        title: "Nike Air Force 1 '07 Men's Shoes.",
+        price: '5320000',
         image01: product_07_image_01,
         image02: product_07_image_02,
-        categorySlug: "ao-somi",
+        categorySlug: "nike-shoes",
         colors: ["white", "red", "orange", "blue"],
-        slug: "ao-somi-caro-07",
-        size: ["l", "xl"]
+        slug: "nike-force-07",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo somi dài tay 08",
-        price: '194000',
+        title: "Nike Air Force 1 '07 LV8 Men's Shoes.",
+        price: '3300000',
         image01: product_08_image_01,
         image02: product_08_image_02,
-        categorySlug: "ao-somi",
+        categorySlug: "nike-shoes",
         colors: ["white", "red", "black"],
-        slug: "ao-somi-dai-tay-08",
-        size: ["s", "m", "xl"]
+        slug: "nike-force-lv8",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo somi tay dài 09",
-        price: '194000',
+        title: "Nike Air Force 1 Low N7 Shoes.",
+        price: '4320000',
         image01: product_09_image_01,
         image02: product_09_image_02,
-        categorySlug: "ao-somi",
+        categorySlug: "nike-shoes",
         colors: ["white", "blue"],
-        slug: "ao-somi-tay-dai-09",
-        size: ["m"]
+        slug: "nike-jordan-low-n7",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Quần jean phong cách 10",
-        price: '194000',
+        title: "Nike Air Trainer SC High Men's Shoes.",
+        price: '7320000',
         image01: product_10_image_01,
         image02: product_10_image_02,
-        categorySlug: "quan-jean",
+        categorySlug: "nike-shoes",
         colors: ["blue", "black"],
-        slug: "quan-jean-phong-cach-10",
-        size: ["l"]
+        slug: "nike-trainer-rc",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Quần jean 11",
-        price: '194000',
+        title: "Nike Air Trainer 1 SP Men's Shoes.",
+        price: '4000000',
         image01: product_11_image_01,
         image02: product_11_image_02,
-        categorySlug: "quan-jean",
+        categorySlug: "nike-shoes",
         colors: ["blue", "black"],
-        slug: "quan-jean-11",
-        size: ["s", "m", "xl"]
+        slug: "nike-trainer-shoes-men",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Quần jean 12",
-        price: '194000',
+        title: "Nike Air Trainer 1 SP Men's Shoes.",
+        price: '3500000',
         image01: product_12_image_01,
         image02: product_12_image_02,
-        categorySlug: "quan-jean",
+        categorySlug: "nike-shoes",
         colors: ["blue"],
-        slug: "quan-jean-12",
-        size: ["s", "m", "xl"]
+        slug: "nike-trainer-men",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo thun Dinosaur 13",
-        price: '189000',
-        image01: product_01_image_01,
-        image02: product_01_image_02,
-        categorySlug: "ao-thun",
+        title: "Nike Air Force 1 Men's Boots.",
+        price: '4500000',
+        image01: product_13_image_01,
+        image02: product_13_image_02,
+        categorySlug: "nike-shoes",
         colors: ["white", "red"],
-        slug: "ao-thun-dinosaur-13",
-        size: ["s", "m", "xl"]
+        slug: "nike-boots",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo thun Dinosaur 14",
-        price: '159000',
-        image01: product_02_image_01,
-        image02: product_02_image_02,
-        categorySlug: "ao-thun",
+        title: "Air Jordan 1 High 85 Release Date.",
+        price: '5750000',
+        image01: product_14_image_01,
+        image02: product_14_image_02,
+        categorySlug: "nike-shoes",
         colors: ["white", "blue"],
-        slug: "ao-thun-dinosaur-14",
-        size: ["s", "m"]
+        slug: "nike-jordan-85",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo thun Dinosaur 15",
-        price: '190000',
-        image01: product_03_image_01,
-        image02: product_03_image_02,
-        categorySlug: "ao-thun",
+        title: "Jordan 1 Mid Little Kids' Shoes.",
+        price: '1500000',
+        image01: product_15_image_01,
+        image02: product_15_image_02,
+        categorySlug: "nike-shoes",
         colors: ["red", "blue"],
-        slug: "ao-thun-dinosaur-15",
-        size: ["xl"]
+        slug: "nike-jordan-mid",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo somi dài tay 16",
-        price: '194000',
-        image01: product_08_image_01,
-        image02: product_08_image_02,
-        categorySlug: "ao-somi",
+        title: "Air Jordan 1 Low Women's Shoe.",
+        price: '2550000',
+        image01: product_16_image_01,
+        image02: product_16_image_02,
+        categorySlug: "nike-shoes",
         colors: ["blue", "black"],
-        slug: "ao-somi-dai-tay-16",
-        size: ["m", "xl"]
+        slug: "nike-jordan-low",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Áo somi tay dài 17",
-        price: '194000',
-        image01: product_09_image_01,
-        image02: product_09_image_02,
-        categorySlug: "ao-somi",
+        title: "Air Jordan 1 Mid SE Women's Shoes.",
+        price: '5330000',
+        image01: product_17_image_01,
+        image02: product_17_image_02,
+        categorySlug: "nike-shoes",
         colors: ["white", "blue"],
-        slug: "ao-somi-tay-dai-17",
-        size: ["s", "l", "xl"]
+        slug: "nike-air-jordan-1-se-women",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
     {
-        title: "Quần jean phong cách 18",
-        price: '194000',
-        image01: product_10_image_01,
-        image02: product_10_image_02,
-        categorySlug: "quan-jean",
+        title: "Jordan 1 Mid SE Little Kids' Shoes.",
+        price: '3270000',
+        image01: product_18_image_01,
+        image02: product_18_image_02,
+        categorySlug: "nike-shoes",
         colors: ["blue", "black"],
-        slug: "quan-jean-phong-cach-18",
-        size: ["s", "m", "l", "xl"]
+        slug: "nike-jordan-1-mid-se-kid",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
     },
-    // 18 products
+    {
+        title: "Air Jordan 1 Mid Big Kids' Shoes.",
+        price: '5270000',
+        image01: product_19_image_01,
+        image02: product_19_image_02,
+        categorySlug: "nike-shoes",
+        colors: ["red", "blue", "black"],
+        slug: "nike-jordan-1-mid-big-kid",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "Air Jordan 1 Zoom Cmft Shoes.",
+        price: '3270000',
+        image01: product_20_image_01,
+        image02: product_20_image_02,
+        categorySlug: "nike-shoes",
+        colors: ["white", "blue", "black", "purple"],
+        slug: "nike-zoom-cmft",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS GRAND COURT SE",
+        price: '2000000',
+        image01: product_21_image_01,
+        image02: product_21_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["white", "blue", "black", "purple"],
+        slug: "adidas-grand-court",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS NMD_R1 PRIMEBLUE",
+        price: '3600000',
+        image01: product_22_image_01,
+        image02: product_22_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["white", "blue", "black", "purple"],
+        slug: "adidas-nmd-primeblue",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS STAN SMITH GOLF",
+        price: '3400000',
+        image01: product_23_image_01,
+        image02: product_23_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["white", "blue", "black", "purple"],
+        slug: "adidas-stsm-golf",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS ULTRABOOST 21 TOKYO RUNNING SHOE",
+        price: '5000000',
+        image01: product_24_image_01,
+        image02: product_24_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["white", "blue", "black", "green"],
+        slug: "adidas-ultraboost-tokyo",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS HARDEN VOL. 6",
+        price: '4000000',
+        image01: product_25_image_01,
+        image02: product_25_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["white", "blue", "gray", "green"],
+        slug: "adidas-harden",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS X9000L2",
+        price: '2850000',
+        image01: product_26_image_01,
+        image02: product_26_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["white", "blue", "black", "purple"],
+        slug: "adidas-x9l2",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS ULTRABOOST DNA CC_1",
+        price: '5000000',
+        image01: product_27_image_01,
+        image02: product_27_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["gray"],
+        slug: "adidas-ultraboost-dna",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS ALPHACOMFY",
+        price: '1800000',
+        image01: product_28_image_01,
+        image02: product_28_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["red"],
+        slug: "adidas-alphacomfy",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS X9000L2 GUARD",
+        price: '2850000',
+        image01: product_29_image_01,
+        image02: product_29_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["blue", "black"],
+        slug: "adidas-guard",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS ALPHATORSION 2.0",
+        price: '3600000',
+        image01: product_30_image_01,
+        image02: product_30_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["black", "white"],
+        slug: "adidas-alphatorsion2",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS NMD_R1",
+        price: '3600000',
+        image01: product_31_image_01,
+        image02: product_31_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["white"],
+        slug: "adidas-nmd-r1",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    {
+        title: "ADIDAS X9000L4",
+        price: '3800000',
+        image01: product_32_image_01,
+        image02: product_32_image_02,
+        categorySlug: "adidas-shoes",
+        colors: ["pink"],
+        slug: "adidas-x9l4",
+        size: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
+    },
+    // 32 products
+
 ]
 
 const getAllProducts = () => products
 
-const getProducts = (count) => {
+function getProducts(count) {
     const max = products.length - count
     const min = 0
     const start = Math.floor(Math.random() * (max - min) + min)
     return products.slice(start, start + count)
 }
 
+
 const productData = {
     getAllProducts,
-    getProducts
+    getProducts,
 }
 
 export default productData
