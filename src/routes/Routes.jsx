60856cf32332ab4b6a0ea9d5b80fb22937aa9df2
NQH1611 <88624093+NQH1611@users.noreply.github.com>
2022-04-04 19:13:11 +0700

    no message

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,7 @@ import Home from '../pages/Home'
 import Catalog from '../pages/Catalog'
 import Product from '../pages/Product'
 import Condition from '../pages/Condition'
+import Contact from '../pages/Contact'
 
 const Routes = () => {
     return (
@@ -14,6 +15,7 @@ const Routes = () => {
             <Route path='/catalog/:slug' component={Product}/>
             <Route path='/catalog' component={Catalog}/>
             <Route path='/condition' component={Condition}/>
+            <Route path="/contact" component={Contact}/>
         </Switch>
     )
 }
